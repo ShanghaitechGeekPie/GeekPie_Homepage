2ccd73700309b842179ac4cb56a5bbec79d4cb9d
zambar <zambar@163.com>
2025-04-23 23:46:08 +0800

    feat: new description

--- src/components/bento.tsx
+++ src/components/bento.tsx
@@ -16,8 +16,8 @@ const features = [
   {
     Icon: RocketIcon,
     name: "信息学院最具影响力的学生组织",
-    description: "我们开发和运营了上科大 OJ、镜像源，上科大 CourseBench 论坛。举办、承办Workshop、AOSCC、ASFR校内比赛、上海市大学生程序设计竞赛等多项赛事和论坛。",
-    href: "/",
+    description: "我们开发和运营了上科大 OJ、镜像源，上科大 CourseBench 论坛。举办、承办 Workshop、AOSCC、ASFR 校内比赛、上海市大学生程序设计竞赛等多项赛事和论坛。",
+    href: "#",
     cta: "Learn more",
     background: (<Services className="absolute h-full transition-all opacity-50 hover:opacity-20 hover:scale-105 [mask-image:linear-gradient(to_top,transparent_0%,#000_100%)]"/>),
     className: "lg:col-start-1 lg:col-end-2 lg:row-start-1 lg:row-end-3",
@@ -27,7 +27,7 @@ const features = [
     name: "信息学院最多样化的学生组织",
     description:
       "我们的成员来自 CS/EE/BME 等多个专业，掌握多种编程语言和技术栈，拥有丰富的项目和比赛经验。在 HPC、CTF、ICPC 等竞赛中屡获殊荣。",
-    href: "/",
+    href: "#",
     cta: "Learn more",
     background: <TechStacks className="absolute opacity-60"/>,
     className: "lg:col-start-1 lg:col-end-2 lg:row-start-3 lg:row-end-4",
@@ -35,8 +35,8 @@ const features = [
   {
     Icon: PieChartIcon,
     name: "信息学院最优秀的学生组织",
-    description: "我们的成员在 MIT、UCB、CMU、UIUC 等世界顶尖高校深造，并发表多篇一作文章于世界顶级会议。",
-    href: "/",
+    description: "我们的元老人物相继创立公司，融资金额达到千万级至亿级人民币，其中一位入选 30 Under 30 榜单。我们的成员在 MIT、UCB、CMU、UIUC 等世界顶尖高校深造，并发表多篇一作文章于世界顶级会议。",
+    href: "#",
     cta: "Learn more",
     background: (<FlickeringGrid className="absolute opacity-30 dark:invert-100"/>),
     className: "lg:col-start-2 lg:col-end-2 lg:row-start-1 lg:row-end-2",
@@ -45,16 +45,16 @@ const features = [
     Icon: GlobeIcon,
     name: "信息学院社会化程度最佳的学生组织",
     description: "我们的成员在多家顶级企业实习和工作，与多家量化、科技企业举办技术分享会等活动。“在这里，我们玩真的。”",
-    href: "/",
+    href: "#",
     cta: "Learn more",
-    background: (<Companies className="absolute border-none transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_0%,#000_100%)] group-hover:scale-105" />),
+    background: (<Companies className="absolute border-none transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_0%,#000_50%)] group-hover:scale-105" />),
     className: "lg:col-start-2 lg:col-end-2 lg:row-start-2 lg:row-end-4",
   },
 ];
 
 export function BentoAbout({ className }: { className?: string }) {
     return (
-        <BentoGrid className={cs(`lg:grid-rows-[200px_minmax(200px,1fr)_200px]`, className)}>
+        <BentoGrid className={cs(`lg:grid-rows-[280px_minmax(200px,1fr)_200px]`, className)}>
             {features.map((feature) => (
                 <BentoCard key={feature.name} {...feature} />
             ))}
