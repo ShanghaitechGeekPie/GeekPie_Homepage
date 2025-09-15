c741ba8c3b36457a01c925becd24ec3b80fb9cf3
zambar <zambar@shanghaitech.edu.cn>
2025-09-16 02:59:44 +0800

    fix: layout

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -8,7 +8,7 @@ export default function PeoplesPage() {
     <>
       <h1 className="text-6xl font-bold my-5">Meet GeekPie</h1>
 
-      <div className="mx-10">
+      <div className="mx-5">
         <section className="py-5 md:py-10">
           <div className="mx-auto max-w-5xl border-t px-6">
             <div className="mt-12 gap-4 sm:grid sm:grid-cols-2 md:mt-24">
@@ -16,16 +16,16 @@ export default function PeoplesPage() {
                 <h2 className="text-3xl font-bold sm:text-4xl">Leadership</h2>
               </div>
               <div className="mt-6 sm:mt-0">
-                <p>江山代有才人出。<br/>GeekPie 部门众多，<br/>离不开强而有力的管理层</p>
+                <p>江山代有才人出。<br />GeekPie 部门众多，<br />离不开强而有力的管理层</p>
               </div>
             </div>
             <div className="mt-12 md:mt-24">
               <div className="grid gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
                 {leaders.map((member, index) => (
                   <Link href={member.url} key={index} className="group">
-                    <img className="h-80 w-full rounded-md object-cover object-top transition-all duration-500 group-hover:h-[22.5rem] border-2" src={member.image} alt="team member" width="800" height="800" />
+                    <img className="group-hover:shadow-md h-80 w-full rounded-md object-cover object-top transition-all duration-500 group-hover:h-[22.5rem]" src={member.image} alt="team member" width="800" height="800" />
                     <div className="px-2 pt-2 sm:pb-0 sm:pt-4">
-                      <div className="flex justify-between">
+                      <div className="flex justify-between items-center">
                         <h3 className="text-title text-base font-medium transition-all duration-500 group-hover:tracking-normal group-hover:text-lg group-hover:font-bold">{member.title}</h3>
                         <span className="text-xs group-hover:text-sm transition-all duration-500 ">{member.handle}</span>
                       </div>
@@ -56,9 +56,9 @@ export default function PeoplesPage() {
               <div className="grid gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
                 {alumni.map((member, index) => (
                   <Link href={member.url} key={index} className="group overflow-hidden">
-                    <img className="h-80 w-full rounded-md object-cover object-top transition-all duration-500 group-hover:h-[22.5rem]" src={member.image} alt="team member" width="800" height="800" />
+                    <img className="group-hover:shadow-md h-80 w-full rounded-md object-cover object-top transition-all duration-500 group-hover:h-[22.5rem]" src={member.image} alt="team member" width="800" height="800" />
                     <div className="px-2 pt-2 sm:pb-0 sm:pt-4">
-                      <div className="flex justify-between">
+                      <div className="flex justify-between items-center">
                         <h3 className="text-title text-base font-medium transition-all duration-500 group-hover:tracking-wide group-hover:text-lg group-hover:font-bold">{member.title}</h3>
                         <span className="text-xs group-hover:text-sm transition-all duration-500 ">{member.handle}</span>
                       </div>
