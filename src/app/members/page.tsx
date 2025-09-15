f041d0b915f7d34a2bb2302d386a364e03b3d244
zambar <zambar@shanghaitech.edu.cn>
2025-09-16 01:57:01 +0800

    feat: new style

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -2,33 +2,81 @@
 
 import ChromaGrid from '@/components/ChromaGrid/ChromaGrid';
 import { leaders, alumni } from '@/statics/members';
+import Link from 'next/link';
 
 export default function PeoplesPage() {
   return (
     <>
-      <h1 className="text-6xl font-bold mb-12">Meet GeekPie</h1>
+      <h1 className="text-6xl font-bold my-5">Meet GeekPie</h1>
 
-      <h2 className="text-5xl font-bold mb-12">Leadership</h2>
-      <div style={{ height: 'auto', position: 'relative' }}>
-        <ChromaGrid
-          items={leaders}
-          radius={300}
-          damping={0.45}
-          fadeOut={0.6}
-          ease="power3.out"
-          />
+      <div className="mx-10">
+        <section className="py-5 md:py-10">
+          <div className="mx-auto max-w-5xl border-t px-6">
+            <div className="mt-12 gap-4 sm:grid sm:grid-cols-2 md:mt-24">
+              <div className="sm:w-2/5">
+                <h2 className="text-3xl font-bold sm:text-4xl">Leadership</h2>
+              </div>
+              <div className="mt-6 sm:mt-0">
+                <p>GeekPie 部门众多，<br/>来认识 GeekPie 管理层</p>
+              </div>
+            </div>
+            <div className="mt-12 md:mt-24">
+              <div className="grid gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
+                {leaders.map((member, index) => (
+                  <Link href={member.url} key={index} className="group overflow-hidden">
+                    <img className="h-80 w-full rounded-md object-cover object-top transition-all duration-500 group-hover:h-[22.5rem]" src={member.image} alt="team member" width="826" height="1239" />
+                    <div className="px-2 pt-2 sm:pb-0 sm:pt-4">
+                      <div className="flex justify-between">
+                        <h3 className="text-title text-base font-medium transition-all duration-500 group-hover:tracking-normal group-hover:text-lg group-hover:font-bold">{member.title}</h3>
+                        <span className="text-xs group-hover:text-sm transition-all duration-500 ">{member.handle}</span>
+                      </div>
+                      <div className="mt-1 flex items-center justify-between">
+                        <span className="text-muted-foreground inline-block text-sm transition duration-300">{member.subtitle}</span>
+                        <div className="group-hover:text-primary-600 dark:group-hover:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 hover:underline group-hover:translate-y-0 group-hover:opacity-100">
+                          Link
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="py-5 md:py-10">
+          <div className="mx-auto max-w-5xl border-t px-6">
+            <div className="mt-12 gap-4 sm:grid sm:grid-cols-2 md:mt-24">
+              <div className="sm:w-2/5">
+                <h2 className="text-3xl font-bold sm:text-4xl">Alumni</h2>
+              </div>
+              <div className="mt-6 sm:mt-0">
+                <p>GeekPie 学生社团自 2014 年创立以来，<br />无数的优秀人才从这里走出，<br />成为各行各业的佼佼者。</p>
+              </div>
+            </div>
+            <div className="mt-12 md:mt-24">
+              <div className="grid gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3">
+                {alumni.map((member, index) => (
+                  <Link href={member.url} key={index} className="group overflow-hidden">
+                    <img className="h-80 w-full rounded-md object-cover object-top transition-all duration-500 group-hover:h-[22.5rem]" src={member.image} alt="team member" width="826" height="1239" />
+                    <div className="px-2 pt-2 sm:pb-0 sm:pt-4">
+                      <div className="flex justify-between">
+                        <h3 className="text-title text-base font-medium transition-all duration-500 group-hover:tracking-wide group-hover:text-lg group-hover:font-bold">{member.title}</h3>
+                        <span className="text-xs group-hover:text-sm transition-all duration-500 ">{member.handle}</span>
+                      </div>
+                      <div className="mt-1 flex items-center justify-between">
+                        <span className="text-muted-foreground inline-block text-sm transition duration-300">{member.subtitle}</span>
+                        <div className="group-hover:text-primary-600 dark:group-hover:text-primary-400 inline-block translate-y-8 text-sm tracking-wide opacity-0 transition-all duration-500 hover:underline group-hover:translate-y-0 group-hover:opacity-100">
+                          Link
+                        </div>
+                      </div>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
-      <h2 className="text-5xl font-bold mb-12" style={{ paddingTop: '20px' }}>Alumni</h2>
-      <div style={{ height: 'auto', position: 'relative' }}>
-        <ChromaGrid
-          items={alumni}
-          radius={300}
-          damping={0.45}
-          fadeOut={0.6}
-          ease="power3.out"
-          />
-      </div>
-
       {/* <h2 className="text-5xl font-bold my-12">alumni</h2>       */}
     </>
   );
--- src/statics/members.tsx
+++ src/statics/members.tsx
@@ -27,7 +27,7 @@ export
     {
       image: "https://avatars.githubusercontent.com/u/27360275?v=4",
       title: "Zebang He",
-      subtitle: "GeekPie 主席 / GeekPie_HPC & GeekPie_CTF 负责人",
+      subtitle: "GeekPie 主席 / GeekPie_HPC & GeekPie_CTF 负责人 | Intern @ SJTU IPADS, UbiQuant",
       handle: "@HeZeBang",
       gradient: "linear-gradient(165deg,#F59E0B,#000)",
       url: "https://github.com/HeZeBang"
@@ -35,7 +35,7 @@ export
     {
       image: "https://avatars.githubusercontent.com/u/25235514?v=4",
       title: "Sizhe Zhao",
-      subtitle: "GeekPie 副主席 / 镜像站 SRE / GeekPie_DevOps 负责人",
+      subtitle: "GeekPie 副主席 / 镜像站 SRE / GeekPie_DevOps 负责人 | Intern @ UbiQuant",
       handle: "@Prince213",
       gradient: "linear-gradient(180deg, #10B981, #000)",
       url: "https://github.com/Prince213"
@@ -43,7 +43,7 @@ export
     {
       image: "https://avatars.githubusercontent.com/u/47201556?v=4",
       title: "Hengyu Ai",
-      subtitle: "GeekPie 副主席",
+      subtitle: "GeekPie 副主席 | Intern @ UC State",
       handle: "@AstatineAi",
       gradient: "linear-gradient(145deg, #3B82F6, #000)",
       url: "https://github.com/AstatineAi"
@@ -51,7 +51,7 @@ export
     {
       image: "https://avatars.githubusercontent.com/u/24501840?v=4",
       title: "Letong Han",
-      subtitle: "GeekPie_Arch 负责人",
+      subtitle: "GeekPie_Arch 负责人 | NSCSCC 2025 Rank 1 | BOSC",
       handle: "@DFPMTS",
       gradient: "linear-gradient(225deg,#8B5CF6,#000)",
       url: "https://github.com/DFPMTS"
