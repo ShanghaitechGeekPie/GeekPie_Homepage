b417012ffba0b84ec1ab39d6242eeef1602e3f0e
Lei Huang <48151038+Winlere@users.noreply.github.com>
2025-09-18 17:02:23 +0000

    feat: avoid controversial desc

--- src/statics/home.tsx
+++ src/statics/home.tsx
@@ -14,7 +14,7 @@ export const features = [
     {
       Icon: RocketIcon,
       name: "信息学院最具影响力的学生组织",
-      description: "我们开发和运营了上科大 OJ、镜像源，上科大 CourseBench 论坛。举办、承办 Workshop、AOSCC、ASFR 校内比赛、上海市大学生程序设计竞赛等多项赛事和论坛。",
+      description: "我们开发和运营了上科大 OJ、镜像源，上科大 CourseBench 论坛；举办、承办 Workshop、AOSCC、ASFR 校内比赛、上海市大学生程序设计竞赛等多项赛事和论坛。",
       href: "#",
       cta: "Learn more",
       background: (<Services className="absolute h-full transition-all opacity-50 hover:opacity-20 hover:scale-105 [mask-image:linear-gradient(to_top,transparent_0%,#000_100%)]"/>),
@@ -24,7 +24,7 @@ export const features = [
       Icon: MixIcon,
       name: "信息学院最多样化的学生组织",
       description:
-        "我们的成员来自 CS/EE/BME 等多个专业，掌握多种编程语言和技术栈，拥有丰富的项目和比赛经验。在 HPC、CTF、ICPC 等竞赛中屡获殊荣。",
+        "我们的成员来自 CS/EE/BME 等多个专业，尊重不同的背景和身份；我们用多样的技术栈，在各类不同竞赛(HPC、CTF、ICPC、IC Design)中屡获殊荣。",
       href: "/members",
       cta: "Members",
       background: <TechStacks className="absolute opacity-60"/>,
@@ -33,7 +33,7 @@ export const features = [
     {
       Icon: PieChartIcon,
       name: "信息学院最优秀的学生组织",
-      description: "我们的元老人物相继创立公司，融资金额达到千万级至亿级人民币，其中一位入选 30 Under 30 榜单。我们的成员在 MIT、UCB、CMU、UIUC 等世界顶尖高校深造，并发表多篇一作文章于世界顶级会议。",
+      description: "我们的元老人物相继创立公司，融资金额达到千万级至亿级人民币，其中一位入选 30 Under 30 榜单。我们的成员在 Stanford、UCB、UToronto、UIUC 等世界顶尖高校深造，并发表一作文章于世界顶级会议。",
       href: "#",
       cta: "Learn more",
       background: (<FlickeringGrid className="absolute opacity-30 dark:invert-100"/>),
@@ -42,7 +42,7 @@ export const features = [
     {
       Icon: GlobeIcon,
       name: "信息学院社会化程度最佳的学生组织",
-      description: "我们的成员在多家顶级企业实习和工作，与多家量化、科技企业举办技术分享会等活动。“在这里，我们玩真的。”",
+      description: "我们的成员在多家顶级企业实习和工作；我们与社会各界建立频繁的联系与合作关系。“在这里，我们玩真的。”",
       href: "#",
       cta: "Learn more",
       background: (<Companies className="absolute border-none transition-all duration-300 ease-out [mask-image:linear-gradient(to_top,transparent_0%,#000_50%)] group-hover:scale-105" />),
