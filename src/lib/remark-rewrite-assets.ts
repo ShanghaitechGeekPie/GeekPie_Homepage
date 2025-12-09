945b025c8b93cdaf5aa2db37340cc0d85f48ada4
zambar <zambar@shanghaitech.edu.cn>
2025-12-10 04:52:01 +0800

    fix: deprecated unist

--- src/lib/remark-rewrite-assets.ts
+++ src/lib/remark-rewrite-assets.ts
@@ -1,41 +1,27 @@
 import { visit } from 'unist-util-visit';
-import type { Node } from 'unist';
-
-interface ImageNode extends Node {
-  type: 'image';
-  url: string;
-  alt?: string;
-}
-
-interface LinkNode extends Node {
-  type: 'link';
-  url: string;
-  children?: Node[];
-}
+import type { Image, Link, Root } from 'mdast';
 
 export function remarkRewriteAssets() {
-  return (tree: Node) => {
+  return (tree: Root) => {
     // Rewrite Images
-    visit(tree, 'image', (node: any) => {
-      const imageNode = node as ImageNode;
-      if (imageNode.url.startsWith('../res/')) {
-        imageNode.url = imageNode.url.replace('../res/', '/res/');
-      } else if (imageNode.url.startsWith('./res/')) {
-        imageNode.url = imageNode.url.replace('./res/', '/res/');
-      } else if (imageNode.url.startsWith('res/')) {
-        imageNode.url = imageNode.url.replace('res/', '/res/');
+    visit(tree, 'image', (node: Image) => {
+      if (node.url.startsWith('../res/')) {
+        node.url = node.url.replace('../res/', '/res/');
+      } else if (node.url.startsWith('./res/')) {
+        node.url = node.url.replace('./res/', '/res/');
+      } else if (node.url.startsWith('res/')) {
+        node.url = node.url.replace('res/', '/res/');
       }
     });
 
     // Rewrite Links (for attachments, pdfs, etc.)
-    visit(tree, 'link', (node: any) => {
-      const linkNode = node as LinkNode;
-      if (linkNode.url.startsWith('../res/')) {
-        linkNode.url = linkNode.url.replace('../res/', '/res/');
-      } else if (linkNode.url.startsWith('./res/')) {
-        linkNode.url = linkNode.url.replace('./res/', '/res/');
-      } else if (linkNode.url.startsWith('res/')) {
-        linkNode.url = linkNode.url.replace('res/', '/res/');
+    visit(tree, 'link', (node: Link) => {
+      if (node.url.startsWith('../res/')) {
+        node.url = node.url.replace('../res/', '/res/');
+      } else if (node.url.startsWith('./res/')) {
+        node.url = node.url.replace('./res/', '/res/');
+      } else if (node.url.startsWith('res/')) {
+        node.url = node.url.replace('res/', '/res/');
       }
     });
   };
