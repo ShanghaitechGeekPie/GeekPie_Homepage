4f5ff66058975c6938a9aa37de218d43cdecefa0
zambar <zambar@shanghaitech.edu.cn>
2025-12-28 22:30:30 +0800

    feat: add robots.txt and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,40 @@
+import { getSortedPostsData } from "@/lib/posts";
+import { links, services } from "@/statics/links";
+import type { MetadataRoute } from "next";
+
+export const dynamic = "force-static";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://geekpie.club/",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    ...links.map((nav) => ({
+        url: `https://geekpie.club${nav.href}`,
+        lastModified: new Date(),
+        changeFrequency: "weekly" as const,
+        priority: 0.9,
+    })),
+    ...getSortedPostsData("event").slice(0, 2).map((post) => ({
+        url: `https://geekpie.club/posts/${post.type}/${post.slug}`,
+        lastModified: new Date(),
+        changeFrequency: "daily" as const,
+        priority: 0.8,
+    })),
+    ...getSortedPostsData("blog").slice(0, 2).map((post) => ({
+        url: `https://geekpie.club/posts/${post.type}/${post.slug}`,
+        lastModified: new Date(),
+        changeFrequency: "daily" as const,
+        priority: 0.8,
+    })),
+    ...services.map((nav) => ({
+        url: `https://geekpie.club${nav.href}`,
+        lastModified: new Date(),
+        changeFrequency: "weekly" as const,
+        priority: 0.7,
+    })),
+  ];
+}
